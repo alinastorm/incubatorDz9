104d7b3710c153e6d5e19b87748a347e7c03290d
AlexGr <753464@gmail.com>
2022-11-19 20:49:47 +0300

    1.0.4

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
+(async () => {
+    await import('./_common/services/mongoDb-service/mongoDbClient');
+    await import("./_common/services/email-service/gmail-adapter");
+    await import("./_common/services/periodicTasks-service");
+    await import('./_common/services/http-service/http-service');
 
-await import('./_common/services/mongoDb-service/mongoDbClient');
-await import("./_common/services/email-service/gmail-adapter");
-await import("./_common/services/periodicTasks-service");
-await import('./_common/services/http-service/http-service');
+})()
 
 
-export { };
