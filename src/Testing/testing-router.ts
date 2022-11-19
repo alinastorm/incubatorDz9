547fa1d93d7a5d81ad90a2402e023bce1992341a
AlexGr <753464@gmail.com>
2022-11-19 20:42:48 +0300

    1.0.3

--- src/Testing/testing-router.ts
+++ src/Testing/testing-router.ts
@@ -1,20 +1,25 @@
-import express, { Request } from 'express';
+import express, { Request, Response } from 'express';
 import testingController from './testing-controller';
 
 
 export const testingRouter = express.Router()
 
 
+testingRouter.get('/',
+    (req: Request, res: Response) => {
+res.send("Hallo Samurai")
+    }
+)
 testingRouter.delete('/testing/all-data',
     testingController.deleteAll,
 )
 testingRouter.all("*", (req: Request, res, next) => {
     if (req.url === "/auth/registration") {
         console.log("*****************************************");
-        console.log('\x1b[36m%s\x1b[0m','req.method:', req.method,req.url);
-        console.log('\x1b[32m%s\x1b[0m','req.body:', req.body);
-        console.log('\x1b[31m%s\x1b[0m','req.cookies:', req.cookies);
-        console.log('\x1b[33m%s\x1b[0m','req.headers:', JSON.stringify(req.headers));
+        console.log('\x1b[36m%s\x1b[0m', 'req.method:', req.method, req.url);
+        console.log('\x1b[32m%s\x1b[0m', 'req.body:', req.body);
+        console.log('\x1b[31m%s\x1b[0m', 'req.cookies:', req.cookies);
+        console.log('\x1b[33m%s\x1b[0m', 'req.headers:', JSON.stringify(req.headers));
         console.log("*****************************************");
     }
 
