c4df8a5177d217f6b69166b5a61e9e0de09343c4
AlexGr <753464@gmail.com>
2022-11-19 21:06:37 +0300

    1.0.6

--- src/_common/services/http-service/http-service.ts
+++ src/_common/services/http-service/http-service.ts
@@ -19,7 +19,7 @@ class HttpService {
     app: core.Express = express()
     httpServer!: http.Server
     httpsServer!: https.Server
-    httpPort: number | string = process.env.HTTP_PORT || 80
+    httpPort: number | string = process.env.PORT || process.env.HTTP_PORT || 80
     httpsPort: number | string = process.env.HTTPS_PORT || 443
 
     //async constructor
@@ -28,8 +28,8 @@ class HttpService {
         try {
             this.setMiddlewares()
             this.setRoutes()
-            this.runHttpsServer()
             this.runHttpServer()
+            // this.runHttpsServer()
             resolve()
         } catch (error) {
             this.stopServer()
